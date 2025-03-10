ae9c9767b8ef31375aebd945534239e405012e2f
mkurzyna <kurzynama@office.umcs.pl>
2025-03-10 15:28:25 +0100

    macie

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Point start = new Point(0,40);
+        Point end = new Point(30,70);
+        Segment line = new Segment(start, end);
+        start.setY(200);
+
+    }
+}
+
+
+
+
+
+
+
+
