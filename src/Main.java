24c2c72c4f71d0510f09d83b87402b8bd9a071cd
mkurzyna <kurzynama@office.umcs.pl>
2025-03-24 16:18:19 +0100

    lab4

--- src/Main.java
+++ src/Main.java
@@ -14,12 +14,15 @@ public class Main {
         points.add(new Point(0,0));
         points.add(new Point(20,50));
         points.add(new Point(15, 80));
+        SvgScene picture = new SvgScene(1000,10000);
         Shape poly = new Polygon(points, new Style("#FF0000","#00FFFF",2));
         Shape ellipse = new Circle(new Style("#FF8888","#000000", 3),
-                new Point(100,100),100);
+                new Point(200,100),30);
+        picture.getShapes().add(poly);
+        picture.getShapes().add(ellipse);
         try{
             FileWriter fw = new FileWriter("ksztalty.svg");
-            fw.write(ellipse.toSvg());
+            fw.write(picture.toSvg());
             fw.close();
         }catch (IOException e){
             System.out.println("Not hello");
